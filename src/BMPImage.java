19607ed850016ae137c50f1dba4a4125d9e86e2c
nickhols2@gmail.com <nickhols2@gmail.com>
2021-08-28 12:01:48 -0400

    Outputs Bitmap with alpha support

--- src/BMPImage.java
+++ src/BMPImage.java
@@ -32,27 +32,83 @@ public class BMPImage extends Image {
                 System.out.println("Unrecognized file extension.");
                 return;
             }
+//            fout = new FileOutputStream(outputFilePath);
+//            byte[] writeBuffer = integerToBytesLittleEndian(0x4D42, 2);
+//            fout.write(writeBuffer);
+//            //fout.write(integerToBytesLittleEndian(0x3A, 4));
+//            int dataSize = height * (width * 3 + (4 - ((width * 3) % 4) % 4));
+//            int fileSize = 14 + 40 + dataSize;
+//            //
+//            dataSize = height * width * 4;
+//            fileSize = 14 + 108 + dataSize;
+//            //
+//            fout.write(integerToBytesLittleEndian(fileSize, 4));
+//            fout.write(integerToBytesLittleEndian(0, 4));
+//            fout.write(integerToBytesLittleEndian(14+40, 4));
+//            fout.write(integerToBytesLittleEndian(40,4));
+//            fout.write(integerToBytesLittleEndian(width,4));
+//            fout.write(integerToBytesLittleEndian(height,4));
+//            fout.write(integerToBytesLittleEndian(1,2));
+//            //fout.write(integerToBytesLittleEndian(24,2));
+//            //
+//            fout.write(integerToBytesLittleEndian(32,2));
+//            fout.write(integerToBytesLittleEndian(3,4));
+//            //
+//            //fout.write(integerToBytesLittleEndian(0,4));
+//            fout.write(integerToBytesLittleEndian(0,4));
+//            fout.write(integerToBytesLittleEndian(0x2e23,4));
+//            fout.write(integerToBytesLittleEndian(0x2e23,4));
+//            fout.write(integerToBytesLittleEndian(0,4));
+//            fout.write(integerToBytesLittleEndian(0,4));
+//            stride = width * 3 + (4 - (width * 3) % 4) % 4;
+//            //
+//            stride = width * 4 + (4 - (width * 4) % 4) % 4;
+//            //
+//            scanlines = new byte[height * stride];
             fout = new FileOutputStream(outputFilePath);
             byte[] writeBuffer = integerToBytesLittleEndian(0x4D42, 2);
             fout.write(writeBuffer);
             //fout.write(integerToBytesLittleEndian(0x3A, 4));
             int dataSize = height * (width * 3 + (4 - ((width * 3) % 4) % 4));
             int fileSize = 14 + 40 + dataSize;
+            //
+            dataSize = height * width * 4;
+            fileSize = 14 + 108 + dataSize;
+            //
             fout.write(integerToBytesLittleEndian(fileSize, 4));
             fout.write(integerToBytesLittleEndian(0, 4));
-            fout.write(integerToBytesLittleEndian(14+40, 4));
-            fout.write(integerToBytesLittleEndian(40,4));
+            fout.write(integerToBytesLittleEndian(14+108, 4));
+            fout.write(integerToBytesLittleEndian(108,4));
             fout.write(integerToBytesLittleEndian(width,4));
             fout.write(integerToBytesLittleEndian(height,4));
             fout.write(integerToBytesLittleEndian(1,2));
-            fout.write(integerToBytesLittleEndian(24,2));
-            fout.write(integerToBytesLittleEndian(0,4));
-            fout.write(integerToBytesLittleEndian(0,4));
+            //fout.write(integerToBytesLittleEndian(24,2));
+            //
+            fout.write(integerToBytesLittleEndian(32,2));
+            fout.write(integerToBytesLittleEndian(3,4));
+            //
+            //fout.write(integerToBytesLittleEndian(0,4));
+            fout.write(integerToBytesLittleEndian(dataSize,4));
             fout.write(integerToBytesLittleEndian(0x2e23,4));
             fout.write(integerToBytesLittleEndian(0x2e23,4));
             fout.write(integerToBytesLittleEndian(0,4));
             fout.write(integerToBytesLittleEndian(0,4));
+            fout.write(integerToBytesLittleEndian(0xFF0000, 4));
+            fout.write(integerToBytesLittleEndian(0xFF00, 4));
+            fout.write(integerToBytesLittleEndian(0xFF, 4));
+            fout.write(integerToBytesLittleEndian(0xFF000000, 4));
+            fout.write(integerToBytesLittleEndian(0x57696E20, 4));
+            for(int i = 0; i < 36; i++){
+                fout.write(integerToBytesLittleEndian(0,1));
+            }
+            fout.write(integerToBytesLittleEndian(0,4));
+            fout.write(integerToBytesLittleEndian(0,4));
+            fout.write(integerToBytesLittleEndian(0,4));
+
             stride = width * 3 + (4 - (width * 3) % 4) % 4;
+            //
+            stride = width * 4 + (4 - (width * 4) % 4) % 4;
+            //
             scanlines = new byte[height * stride];
             int outputBufferIndex = 0;
             for(int i = 0; i < height; i++){
@@ -61,6 +117,10 @@ public class BMPImage extends Image {
                     scanlines[outputBufferIndex++] = (byte)(pixels[(height - i) - 1][j][2]);
                     scanlines[outputBufferIndex++] = (byte)(pixels[(height - i) - 1][j][1]);
                     scanlines[outputBufferIndex++] = (byte)(pixels[(height - i) - 1][j][0]);
+                    //
+                    scanlines[outputBufferIndex++] = (byte)(pixels[(height - i) - 1][j][3]);
+                    bytesWritten += 1;
+                    //
                     bytesWritten += 3;
                 }
                 outputBufferIndex += (4 - (bytesWritten % 4)) % 4;
